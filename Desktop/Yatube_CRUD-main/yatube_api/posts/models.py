7b551fc4ed41b1ad1821497af959db6970fb75a3
Alex <Magnatov2020@mail.ru>
2025-11-14 16:20:38 +0300

    Success

---
+++ Desktop/Yatube_CRUD-main/yatube_api/posts/models.py
@@ -0,0 +1,96 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название группы'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Уникальный идентификатор'
+    )
+    description = models.TextField(verbose_name='Описание группы')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(verbose_name='Текст поста')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    image = models.ImageField(
+        upload_to='posts/',
+        null=True,
+        blank=True,
+        verbose_name='Изображение'
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        blank=True,
+        null=True,
+        verbose_name='Группа'
+    )
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ('-pub_date',)
+        indexes = [
+            models.Index(fields=['-pub_date'], name='pub_date_desc_idx'),
+        ]
+
+    def __str__(self):
+        return self.text[:50]
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария'
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пост'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+    created = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата создания'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-created',)
+        indexes = [
+            models.Index(fields=['-created'], name='created_desc_idx'),
+            models.Index(fields=['post'], name='post_idx'),
+        ]
+
+    def __str__(self):
+        return f'Комментарий {self.author} к посту {self.post.id}'
